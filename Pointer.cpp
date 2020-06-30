870ab65dd3848b511724a0ef1132326ab4beebe1
anvithahm <54534166+anvithahm@users.noreply.github.com>
2020-06-30 12:12:24 +0530

    Create Pointer.cpp

---
+++ Pointer.cpp
@@ -0,0 +1,15 @@
+//ADDITION OF TWO NUMBERS USING POINTERS
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int f,s,*p,*q,sum;
+cout<<"Enter two integers to add\n";
+cin>>f>>s;
+p= &f;
+q=&s;
+sum= *p + *q;
+cout<<"Sum of 2 numbers="<< sum;
+getch();
+}
